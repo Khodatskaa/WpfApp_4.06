15c900e47dd4e8b863153728e0408147c0882117
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-06-04 20:35:57 +0300

    Ex 1 was done

--- WpfApp_4.06/MainWindow.xaml.cs
+++ WpfApp_4.06/MainWindow.xaml.cs
@@ -1,24 +1,42 @@
-﻿using System.Text;
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp_4._06
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void StartProcess_Click(object sender, RoutedEventArgs e)
+        {
+            string processPath = "notepad.exe"; 
+            int exitCode = await StartProcessAsync(processPath);
+            CompletionCodeTextBlock.Text = $"Process completed with exit code: {exitCode}";
+        }
+
+        private Task<int> StartProcessAsync(string processPath)
+        {
+            return Task.Run(() =>
+            {
+                try
+                {
+                    Process process = new Process();
+                    process.StartInfo.FileName = processPath;
+                    process.Start();
+                    process.WaitForExit();
+                    return process.ExitCode;
+                }
+                catch (Exception ex)
+                {
+                    Dispatcher.Invoke(() => MessageBox.Show($"Error starting process: {ex.Message}"));
+                    return -1; 
+                }
+            });
         }
     }
 }
