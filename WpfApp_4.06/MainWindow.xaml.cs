61bfaa5367f98ddfb29c8e030fa1b8d84535d0db
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-06-04 20:33:18 +0300

    Ex 4 was done

--- WpfApp_4.06/MainWindow.xaml.cs
+++ WpfApp_4.06/MainWindow.xaml.cs
@@ -1,24 +1,46 @@
-﻿using System.Text;
+﻿using System;
+using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp_4._06
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LaunchNotepad_Click(object sender, RoutedEventArgs e)
+        {
+            LaunchApplication("notepad.exe");
+        }
+
+        private void LaunchCalculator_Click(object sender, RoutedEventArgs e)
+        {
+            LaunchApplication("calc.exe");
+        }
+
+        private void LaunchPaint_Click(object sender, RoutedEventArgs e)
+        {
+            LaunchApplication("mspaint.exe");
+        }
+
+        private void LaunchCustomApp_Click(object sender, RoutedEventArgs e)
+        {
+            LaunchApplication("your_custom_app.exe");
+        }
+
+        private void LaunchApplication(string appPath)
+        {
+            try
+            {
+                Process.Start(appPath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error launching application: {ex.Message}");
+            }
         }
     }
 }
